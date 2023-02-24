7da9e38104ab5a5983be0b362f6b303af24a7dec
Mohammad <moh.hossain98@gmail.com>
2023-02-24 11:15:54 -0500

    code from 2/24

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import "./App.css";
 import Body from "./Body";
 import { useState } from "react";
+import NewStudentForm from "./NewStudentForm";
 
 const student1 = {
   name: "John",
   age: 20,
-  professon: "student",
+  profession: "student",
   code: "JS",
 };
 
 const student2 = {
   name: "Jane",
   age: 21,
-  professon: "developer",
+  profession: "developer",
   code: "JS",
 };
 
 const student3 = {
   name: "Jack",
   age: 22,
-  professon: "photographer",
+  profession: "photographer",
   code: "HTML",
 };
 
@@ -27,9 +28,15 @@ function App() {
   const [count, setCount] = useState(0);
 
   console.log(count, "this is the state");
-  const studentlist = [student1, student2, student3];
+  // const studentlist = [student1, student2, student3];
 
-  const [students, setStudents] = useState(studentlist);
+  const [studentlist, setStudentList] = useState([
+    student1,
+    student2,
+    student3,
+  ]);
+
+  const [students, setStudents] = useState([student1, student2, student3]);
 
   const handleSignIn = () => {
     setCount(count + 1);
@@ -40,6 +47,12 @@ function App() {
     setStudents([]);
   };
 
+  const onStudentListChange = (student) => {
+    // studentlist.push(student);
+    setStudentList([...studentlist, student]);
+    console.log(studentlist);
+    setStudents([...students, student]);
+  };
   return (
     <div className="App">
       <nav>
@@ -49,6 +62,9 @@ function App() {
       </nav>
 
       <h3>You signed in {count} times </h3>
+      <NewStudentForm
+        onStudentListChange={onStudentListChange}
+      ></NewStudentForm>
       <Body students={students}></Body>
     </div>
   );
---
+++ src/NewStudentForm.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+
+function NewStudentForm({ onStudentListChange }) {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [profession, setProfession] = useState("");
+  const [code, setCode] = useState("");
+
+  const handleName = (e) => {
+    console.log(e.target.value);
+    setName(e.target.value);
+  };
+
+  const handleAge = (e) => {
+    console.log(e.target.value);
+    setAge(e.target.value);
+  };
+
+  const handleProfession = (e) => {
+    console.log(e.target.value);
+    setProfession(e.target.value);
+  };
+
+  const handleCode = (e) => {
+    console.log(e.target.value);
+    setCode(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, age, profession, code);
+    const student = { name, age, profession, code };
+    console.log(student);
+    onStudentListChange(student);
+  };
+
+  return (
+    <div>
+      <h1>Add a new student</h1>
+
+      <form
+        className="student-form"
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit}
+      >
+        <label>Name: </label>
+        <input
+          type="text"
+          name="name"
+          onChange={handleName}
+          value={name}
+        ></input>
+        <label>Age: </label>
+        <input
+          type="number"
+          name="age"
+          onChange={handleAge}
+          value={age}
+        ></input>
+        <label>Profession: </label>
+        <input
+          type="text"
+          name="profession"
+          onChange={handleProfession}
+          value={profession}
+        ></input>
+        <label>Code: </label>
+        <input
+          type="text"
+          name="code"
+          onChange={handleCode}
+          value={code}
+        ></input>
+
+        <input type="submit"></input>
+      </form>
+
+      {/* <p>
+        {name}, {age}, {profession}, {code}
+      </p> */}
+    </div>
+  );
+}
+
+export default NewStudentForm;
